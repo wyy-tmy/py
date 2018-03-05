646a827d166ddff67fc3a65ce53dcb79c6f79b21
wyy-tmy <37060109+wyy-tmy@users.noreply.github.com>
2018-03-05 19:58:12 +0800

    bingo版

---
+++ caishu.py
@@ -0,0 +1,13 @@
+from random import randint
+num = randint(1,100)
+print(input)
+passward = False
+while passward == False:
+	daan = int(input())
+	if daan > num:
+		print("no,no,no,too big")
+	if daan < num:
+		print("no,no,no,too small")
+	if daan == num:
+		print("bingo")
+		passward = True
